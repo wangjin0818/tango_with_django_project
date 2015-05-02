f283259d28c576798cd32766537fdf0253428d68
Wang Jin <wangjin0818@gmail.com>
2015-05-02 22:52:00 +0800

    Model commit 1

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+
+	# For Python 2, use __str__ on Python 3
+	def __unicode__(self):
+		return self.name
+
+class Page(models.Model):
+	Category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+
+	# For Python 2, use __str__ on Python 3
+	def __unicode__(self):
+		return self.title
